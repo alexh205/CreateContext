e5dd779504b9ccd339233a0d97b43104c9ef7a40
Alainalvarez14 <39074661+Alainalvarez14@users.noreply.github.com>
2022-10-04 19:10:49 -0400

    Context Creation completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Navbar from "./components/Navbar";
 import SeparatorOne from "./components/SeparatorOne";
 import SideCard from "./components/SideCard";
 
-function App (){
+function App() {
+  // What is this?
   return (
     <>
       <Navbar />
