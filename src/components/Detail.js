52fc980d999d798062d0e1291cbf2e2a2736c4ae
Alex Hunt <95322089+alexh205@users.noreply.github.com>
2022-10-04 19:43:36 -0400

    initial commit

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,17 +2,17 @@ import { useContext } from "react";
 import { HoroscopeContext } from "../context/HoroscopeContext.js";
 
 const Detail = () => {
-  const horoscopesObj = useContext(HoroscopeContext);
-
+  // const { horoscopesObj } = useContext(HoroscopeContext);
+  const { sign } = useContext(HoroscopeContext);
   return (
     <div className="details">
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg"
         alt=""
       />
-      <h2>Current Sign Name</h2>
-      <h4>Element: </h4>
-      <h4>Traits: </h4>
+      <h2 vlaue={sign.name}>{sign.name}</h2>
+      <h4 value={sign.element}>Element: {sign.element}</h4>
+      <h4 value={sign.traits}>Traits: {sign.traits}</h4>
     </div>
   );
 };
--- src/context/HoroscopeContext.js
+++ src/context/HoroscopeContext.js
@@ -1,5 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import horoscopesObj from "../data/horoscopes";
 
 export const HoroscopeContext = createContext();
 
-console.log(HoroscopeContext);
+export const HoroscopeProvider = (props) => {
+  const [currentSign, setCurrentSign] = useState("Virgo");
+  const sign = horoscopesObj[currentSign];
+
+  return (
+    <HoroscopeContext.Provider value={{ sign, setCurrentSign }}>
+      {props.children}
+    </HoroscopeContext.Provider>
+  );
+};
